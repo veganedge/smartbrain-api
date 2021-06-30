33fc5e91fc8df9cc67466869b04eaed5dec2d7c1
veganedge <scottxbrown@yahoo.com>
2021-06-29 21:47:17 -0700

    got signin recognizing hashed passwords

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/signin', (req, res) => {
    db.select('email', 'hash').from('login')
    .where('email', '=', req.body.email)
    .then(data => {
-        const isValid = bcrypt.compareSync(eq.body.password, data[0].hash);
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
         if (isValid) {
             return db.select('*').from('users ')
                 .where('email', '=', req.body.email)
