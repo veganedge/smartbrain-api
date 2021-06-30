0190df9fffa6be394eb5f7be22a57132aff15fbc
veganedge <scottxbrown@yahoo.com>
2021-06-29 18:11:56 -0700

    signin now working

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt-nodejs');  //hashing passwords to securely store in smartbrain database (db)
 const cors = require('cors');
 const knex = require('knex');
-const { response } = require('express');
 
 const db = knex({
     client: 'pg', //postgresql
@@ -19,51 +18,55 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '100',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '101',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.get('/', (req, res)=> {
-    res.send(database.users);
+    res.send('success');
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('no registered users with that email and password combination ');
-    }
+   db.select('email', 'hash').from('login')
+   .where('email', '=', req.body.email)
+   .then(data => {
+        const isValid = bcrypt.compareSync(eq.body.password, data[0].hash);
+        if (isValid) {
+            return db.select('*').from('users ')
+                .where('email', '=', req.body.email)
+                .then( user => {
+                    res.json(user[0])   
+                })
+                .catch(err => res.status(400).json('unable to get user')
+                )
+        } else {
+            res.status(400).json('wrong credentias')
+        }
+   })
+    .catch(err => res.status(400).json('wrong credentials'))
 }) 
 
 app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;     //registers new user on Front End and logs them in.
-    db('users')                                     //not working while trying to register through Postman, throws error.
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
-    })
-        .then(user => {
-            res.json(user[0]);
+    const { email, name, password } = req.body;
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')                         //registers new user on Front End and logs them in.
+            .returning('*')                             //not working while trying to register through Postman, throws error.
+            .insert({
+                email: loginemail[0],
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })     
         .catch(err => res.status(400).json('unable to register')); 
 })
 
@@ -74,7 +77,7 @@ app.get('/profile/:id', (req, res) => {
         if (user.length) {
             res.json(user[0])
         } else {
-            res.status(400).json('not found')
+            res.status(400).json('profile/user not found')
         }
     })
     .catch(err => res.status(400).json('error getting user'))
